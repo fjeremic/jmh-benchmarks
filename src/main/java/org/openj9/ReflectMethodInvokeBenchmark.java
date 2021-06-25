65c08e1fd3afa8f4b979e0bb06c06edee7f15c9c
vchang@us.ibm <seeluser@svlxtor4.svl.ibm.com>
2021-06-25 08:10:05 -0700

    Add ReflectMethodInvokeBenchmark

---
+++ src/main/java/org/openj9/ReflectMethodInvokeBenchmark.java
@@ -0,0 +1,28 @@
+package org.openj9;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+@Fork(value = 1)
+@Warmup(iterations = 3, time = 2000, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 3, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+public class ReflectMethodInvokeBenchmark {
+    public static Method m;
+
+    private static void process(String str) {
+    }
+
+    @Setup
+    public void setup() throws NoSuchMethodException, IllegalAccessException {
+	m = ReflectMethodInvokeBenchmark.class.getDeclaredMethod("process", String.class);
+    }
+
+    @Benchmark
+    public void testInvoke() throws IllegalAccessException, InvocationTargetException {
+	m.invoke(null, "test");
+    }
+}
